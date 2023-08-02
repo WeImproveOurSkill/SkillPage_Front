3c4e3826003ac1a56478db390aeaecf121709d23
benidene <dlgusehd09@naver.com>
2023-08-03 08:30:54 +0900

    <design> : Footer 하단고정 삭제와 11번가 MANGO로 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <Container>
       <div>
-        11번가(주)는 통신판매중개자로서 오픈마켓 11번가의 거래당사자가 아니며, 입점판매자가 등록한
-        상품정보 및 거래에 대해 11번가(주)는 일체 책임을 지지 않습니다.
+        MANGO(주)는 통신판매중개자로서 오픈마켓 MANGO의 거래당사자가 아니며, 입점판매자가 등록한
+        상품정보 및 거래에 대해 MANGO(주)는 일체 책임을 지지 않습니다.
       </div>
     </Container>
   );
@@ -17,8 +17,6 @@ const Container = styled.div`
   align-items: center;
   width: 100%;
   height: 100px;
-  position: relative;
-  transform: translateY(0%);
   background-color: #fffd55;
   color: #25b00d;
 `;

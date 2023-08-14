cf8472e6036e79b338aa4e0db5b9fa1e2f306d85
benidene <111226162+benidene@users.noreply.github.com>
2023-08-14 14:22:49 +0900

    Update useLoginLogic.js
    error fix

--- src/util/useLoginLogic.js
+++ src/util/useLoginLogic.js
@@ -7,10 +7,8 @@ import { checkPassword, checkId, checkEmail, checkPhoneNumber } from './checkPas
 function useLoginLogic(initialInputs, url, alertMsg, key1, key2, key3, key4, key5, key6, key7) {
   const navigate = useNavigate();
   const [inputs, setInputs] = useState(initialInputs);
-
   //   const [usableId, setUsableId] = useState(false);
   //   const [auth, setAuth] = useState(false);
-
   const { setIsLoggedIn, setTokens } = useContext(userContext);
 
   const onChange = (e) => {
@@ -68,8 +66,7 @@ function useLoginLogic(initialInputs, url, alertMsg, key1, key2, key3, key4, key
 
   const onClick = async (e) => {
     e.preventDefault();
-    const resultId = checkId(inputs.userIdentifier);
-    if (!resultId) return;
+
     if (
       inputs[key1] === '' ||
       inputs[key2] === '' ||
@@ -122,7 +119,6 @@ function useLoginLogic(initialInputs, url, alertMsg, key1, key2, key3, key4, key
     }
   };
 
-  return [inputs, onChange, onSubmit, clickCheckId, clickPhoneNumber, clickAuthPhoneNumber];
+  return [inputs, onChange, onClick];
 }
-// , clickCheckId, clickPhoneNumber, clickAuthPhoneNumber
 export default useLoginLogic;

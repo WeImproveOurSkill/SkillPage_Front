f03cad4f100795ffc43202b507940f6a8ebbe132
benidene <dlgusehd09@naver.com>
2023-08-30 19:03:51 +0900

    #14 <feat> : 장바구니 1차 완성

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,6 +1,22 @@
 import { styled } from 'styled-components';
 import Footer from 'components/Footer/Footer';
+import CartItem from './CartItem';
 import MainImg from 'asset/logo192.png';
+
+const DUMMY_PRODUCTS = [
+  {
+    basketId: 'p1',
+    productName: 'My First Book',
+    productPrice: 6,
+    productCnt: 'The first book I ever wrote'
+  },
+  {
+    basketId: 'p2',
+    productName: 'My Second Book',
+    productPrice: 5,
+    productCnt: 'The Second book I ever wrote'
+  }
+];
 
 function Cart() {
   return (
@@ -8,7 +24,17 @@ function Cart() {
       <LogoBox>
         <Logo src={MainImg} alt='MainImg' />
       </LogoBox>
-      <CartContainer></CartContainer>
+      <CartContainer>
+        {DUMMY_PRODUCTS.map((product) => (
+          <CartItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </CartContainer>
       <Footer />
     </Wrapper>
   );
@@ -18,6 +44,7 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: #efefef;
 `;
 
 const LogoBox = styled.div`
@@ -31,7 +58,9 @@ const Logo = styled.img`
 `;
 
 const CartContainer = styled.div`
-  width: 980px;
+  width: 900px;
+  padding: 40px 39px 40px 39px;
+  border: 1px solid gray;
 `;
 
 export default Cart;
---
+++ src/pages/Cart/CartItem.jsx
@@ -0,0 +1,15 @@
+function CartItem() {
+  return (
+    <div>
+      <div>체크박스</div>
+      <div>상품사진</div>
+      <div>
+        <div>상품 이름</div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default CartItem;

04dd8d96e91347920f8583da2b583baee0fee2ca
benidene <dlgusehd09@naver.com>
2023-08-30 18:57:57 +0900

    #15 <refactor> : useContext 삭제 redux toolkit 사용

--- src/util/useLocalStorage.js
+++ src/util/useLocalStorage.js
@@ -1,15 +1,38 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
-function useLocalStorage(key, initialState) {
+// function useLocalStorage(key, initialState) {
+//   const [state, setState] = useState(
+//     () => JSON.parse(window.localStorage.getItem(key)) || initialState
+//   );
+
+//   useEffect(() => {
+//     window.localStorage.setItem(key, JSON.stringify(state));
+//   }, [key, state]);
+
+//   return [state, setState];
+// }
+
+// export default useLocalStorage;
+
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setTokens } from '../store/authSlice';
+
+export function useLocalStorage(key, initialState) {
   const [state, setState] = useState(
     () => JSON.parse(window.localStorage.getItem(key)) || initialState
   );
 
+  const dispatch = useDispatch(); // useDispatch를 추가합니다
+
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+
+    // 토큰 정보 업데이트
+    if (key === 'jwt') {
+      dispatch(setTokens(state));
+    }
+  }, [key, state, dispatch]);
 
   return [state, setState];
 }
-
-export default useLocalStorage;

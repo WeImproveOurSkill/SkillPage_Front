a2699842787da0bfb677a6faf73e5b1f415b8074
benidene <dlgusehd09@naver.com>
2023-09-11 18:49:53 +0900

    #4 <faet> : 하단고정 및 width 값 줄어들때 줄바뀜 방지되게 변경

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,15 +3,15 @@ import { styled } from 'styled-components';
 function Footer() {
   return (
     <Container>
-      <div>
+      <Text>
         MANGO(주)는 통신판매중개자로서 오픈마켓 MANGO의 거래당사자가 아니며, 입점판매자가 등록한
         상품정보 및 거래에 대해 MANGO(주)는 일체 책임을 지지 않습니다.
-      </div>
+      </Text>
     </Container>
   );
 }
 
-const Container = styled.div`
+const Container = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -19,6 +19,16 @@ const Container = styled.div`
   height: 100px;
   background-color: #fffd55;
   color: #25b00d;
+  position: relative;
+  transform: translateY(0%);
+`;
+
+const Text = styled.div`
+  display: flex;
+  white-space: nowrap; // div 내의 줄바꿈 금지
+  justify-content: center;
+  align-items: center;
+  width: 1280px;
 `;
 
 export default Footer;
